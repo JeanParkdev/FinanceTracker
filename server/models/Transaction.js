c9eea635bf2dae037f8a016e7df6a66a06933b58
Jean <thejeanpark@gmail.com>
2026-03-19 16:12:07 -0400

    transaction

---
+++ server/models/Transaction.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+import { useImperativeHandle } from 'react';
+
+const transactionSchema = new mongoose.Schema({
+    userID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    type: {
+        type: String,
+        enum: ['income', 'expense'],
+        required: true,
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    isRecurring: {
+        type: Boolean,
+        default: false,
+    },
+    recurringFreq: {
+        type: String,
+        enum: ['daily', 'weekly', 'monthly', 'yearly'],
+        default: null,
+    },
+    description: {
+        type: String,
+        default: null,
+    },
+});
+
+const Transaction = mongoose.model('Transaction', transactionSchema);
+export default Transaction;
